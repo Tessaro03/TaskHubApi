25aacde2d879771bf48f8df0a0aeb6684f2aef4c
Gabriel Tessaro Favareto <119542621+Tessaro03@users.noreply.github.com>
2023-12-19 17:57:25 -0300

    Adicionado validação de usuario admin para alteração de projeto

--- src/main/java/taskhub/domain/projeto/validacao/validacaoPatch/ValidadorProjetoPatchUsuario.java
+++ src/main/java/taskhub/domain/projeto/validacao/validacaoPatch/ValidadorProjetoPatchUsuario.java
@@ -1,10 +1,11 @@
 package taskhub.domain.projeto.validacao.validacaoPatch;
 
 
+import taskhub.domain.projeto.DadosAlterarProjeto;
 import taskhub.domain.usuario.Usuario;
 
 public interface ValidadorProjetoPatchUsuario{
     
-        void validar(Usuario usuario);
+        void validar(Usuario usuario, DadosAlterarProjeto dados);
 
 }
