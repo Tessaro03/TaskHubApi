6ca37bd2ba2bd10627581954431e4562b5b3cbb8
Gabriel Tessaro Favareto <119542621+Tessaro03@users.noreply.github.com>
2023-12-21 13:56:43 -0300

    Organização dos Validadores de cada metodo

--- src/main/java/taskhub/domain/tarefa/validacao/ValidadorTarefa.java
+++ src/main/java/taskhub/domain/tarefa/validacao/ValidadorTarefa.java
@@ -1,10 +1,43 @@
 package taskhub.domain.tarefa.validacao;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import taskhub.domain.tarefa.DadosAlterarTarefa;
 import taskhub.domain.tarefa.DadosCriacaoTarefa;
+import taskhub.domain.tarefa.validacao.validacaoDelete.ValidadorTarefaDelete;
+import taskhub.domain.tarefa.validacao.validacaoPatch.ValidadorTarefaPatch;
+import taskhub.domain.tarefa.validacao.validacaoPatch.ValidadorTarefaPatchUsuario;
+import taskhub.domain.tarefa.validacao.validacaoPost.ValidadorTarefaPost;
+import taskhub.domain.usuario.Usuario;
 
-public interface ValidadorTarefa {
+@Service
+public class ValidadorTarefa {
     
-        void validar(DadosCriacaoTarefa dados);
+    @Autowired
+    private List<ValidadorTarefaPost> validadorPost;
 
+    @Autowired
+    private List<ValidadorTarefaPatchUsuario> validadorPatchUsuario;
 
+        @Autowired
+    private List<ValidadorTarefaPatch> validadorPatch;
+
+    @Autowired
+    private List<ValidadorTarefaDelete> validadorDelete;
+
+    public void validarPost(DadosCriacaoTarefa dados){
+        validadorPost.forEach(v -> v.validar(dados));
+    }
+
+     public void validarPatch(DadosAlterarTarefa dados, Usuario usuario){
+        validadorPatchUsuario.forEach(v -> v.validar(dados, usuario));
+        validadorPatch.forEach(v -> v.validar(dados));
+    }
+
+    public void validadorDelete(Long id, Usuario usuario){
+        validadorDelete.forEach(v -> v.validar(id, usuario));
+    }
 }
