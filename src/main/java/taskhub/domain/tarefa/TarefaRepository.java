70ee5921e55acc552b1cea6d4de9aad18d1ef384
Gabriel Tessaro Favareto <119542621+Tessaro03@users.noreply.github.com>
2023-12-21 13:56:13 -0300

    Adicionado validações para os metodos

--- src/main/java/taskhub/domain/tarefa/TarefaRepository.java
+++ src/main/java/taskhub/domain/tarefa/TarefaRepository.java
@@ -1,5 +1,7 @@
 package taskhub.domain.tarefa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,9 +15,15 @@ public interface TarefaRepository extends JpaRepository<Tarefa, Long>{
     @Query("DELETE FROM Tarefa t WHERE t.projeto.id = :idProjeto")
     void deletarTarefaIdProjeto(Long idProjeto);
 
-        @Modifying
+    @Modifying
     @Transactional
     @Query("DELETE FROM Tarefa t WHERE t.id = :idTarefa")
     void deletarTarefaIdTarefa(Long idTarefa);
+ 
+    @Query("SELECT t FROM Tarefa t JOIN t.equipe e WHEERE e.usuario.id = :usuarioId ")
+    List<Tarefa> buscarTarefasUsuario(Long usuarioId);
+
+    @Query("SELECT t FROM Tarefa t JOIN t.equipe e WHEERE e.usuario.id = :idUsuario AND t.id = :idTarefa")
+    Tarefa buscarTarefaIdUsuarioId(Long idUsuario, Long idTarefa);
     
 }
