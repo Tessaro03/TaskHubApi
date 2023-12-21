dfc001ccd7ebd25c3fd1d1da388221824d28117d
Gabriel Tessaro Favareto <119542621+Tessaro03@users.noreply.github.com>
2023-12-21 13:55:43 -0300

    Validação criação de membro duplicado

--- src/main/java/taskhub/domain/membro/validacao/ValidacaoMembroDuplicado.java
+++ src/main/java/taskhub/domain/membro/validacao/ValidacaoMembroDuplicado.java
@@ -3,7 +3,6 @@ package taskhub.domain.membro.validacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import jakarta.validation.ValidationException;
 import taskhub.domain.membro.DadosCriacaoMembro;
 import taskhub.domain.membro.MembroRepository;
 import taskhub.infra.excepetion.ValidacaoExcepetion;
