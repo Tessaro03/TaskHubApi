924b067245970879badb393b1c606306a1435778
Gabriel Tessaro Favareto <119542621+Tessaro03@users.noreply.github.com>
2023-12-21 21:12:33 -0300

    Organização de validação para Membro

--- src/main/java/taskhub/domain/membro/validacao/ValidadorMembro.java
+++ src/main/java/taskhub/domain/membro/validacao/ValidadorMembro.java
@@ -1,11 +1,37 @@
 package taskhub.domain.membro.validacao;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import taskhub.domain.membro.DadosCriacaoMembro;
+import taskhub.domain.membro.validacao.validacaoDelete.ValidadorMembroDelete;
+import taskhub.domain.membro.validacao.validacaoPost.ValidadorMembroPost;
+import taskhub.domain.membro.validacao.validacaoPost.ValidadorMembroPostUsuario;
+import taskhub.domain.usuario.Usuario;
 
-public interface ValidadorMembro {
+@Service
+public class ValidadorMembro {
     
 
-            void validar(DadosCriacaoMembro dados);
+    @Autowired 
+    private List<ValidadorMembroPost> validadorPost;
 
+    @Autowired 
+    private List<ValidadorMembroPostUsuario> validadorPostUsuario;
+
+    
+    @Autowired 
+    private List<ValidadorMembroDelete> validadorDelete;
+
+    public void validarPost(DadosCriacaoMembro dados, Usuario usuario){
+        validadorPost.forEach(v -> v.validar(dados));
+        validadorPostUsuario.forEach(v -> v.validar(usuario, dados));
+    }
+
+    public void validarDelete(Long id, Usuario usuario){
+
+        validadorDelete.forEach(v -> v.validar(id, usuario));
+    }
 }
