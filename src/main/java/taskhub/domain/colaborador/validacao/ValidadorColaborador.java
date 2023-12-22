3e74ea5c2d65b79537c72f8e3af4dbc989654472
Gabriel Tessaro Favareto <119542621+Tessaro03@users.noreply.github.com>
2023-12-22 16:54:46 -0300

    Criação da validação do Colaborador

--- src/main/java/taskhub/domain/colaborador/validacao/ValidadorColaborador.java
+++ src/main/java/taskhub/domain/colaborador/validacao/ValidadorColaborador.java
@@ -1,8 +1,39 @@
 package taskhub.domain.colaborador.validacao;
 
-import taskhub.domain.colaborador.DadosCriacaoColaborador;
+import java.util.List;
 
-public interface ValidadorColaborador {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import taskhub.domain.colaborador.DadosAlterarColaborador;
+import taskhub.domain.colaborador.DadosCriacaoColaborador;
+import taskhub.domain.colaborador.validacao.validacaoDelete.ValidadorColaboradorDelete;
+import taskhub.domain.colaborador.validacao.validacaoPatch.ValidadorColaboradorPatch;
+import taskhub.domain.colaborador.validacao.validacaoPost.ValidadorColaboradorPost;
+import taskhub.domain.usuario.Usuario;
+
+@Service
+public class ValidadorColaborador {
     
-    void validar(DadosCriacaoColaborador dados);
+    @Autowired
+    private List<ValidadorColaboradorPost> validadorPost;
+
+    @Autowired
+    private List<ValidadorColaboradorDelete> validadorDelete;
+
+    @Autowired
+    private List<ValidadorColaboradorPatch> validadorPatch;
+
+    public void validarPost(DadosCriacaoColaborador dados){
+        validadorPost.forEach( v -> v.validar(dados));;
+    }
+
+    public void validarDelete(Long id, Usuario usuario) {
+        validadorDelete.forEach(v -> v.validar(id, usuario));
+    }
+
+    public void validadorPatch(DadosAlterarColaborador dados, Usuario usuario){
+        validadorPatch.forEach(v -> v.validar(dados, usuario));
+    }
+
 }
